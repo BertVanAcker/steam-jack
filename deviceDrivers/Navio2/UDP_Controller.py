cf69e5b590ce2d8c00ce669b7140101d67f5d782
bva.bmkr <bva.bmkr@gmail.com>
2022-01-28 16:24:16 +0100

    --string constants

--- deviceDrivers/Navio2/UDP_Controller.py
+++ deviceDrivers/Navio2/UDP_Controller.py
@@ -14,14 +14,14 @@ import navio.util
 NAVIO_ActionLED = "LED"
 
 # LED colors
-NAVIO_LED_Black = 0
-NAVIO_LED_Red = 1
-NAVIO_LED_Green = 2
-NAVIO_LED_Blue = 3
-NAVIO_LED_Yellow = 4
-NAVIO_LED_Cyan = 5
-NAVIO_LED_Magenta = 6
-NAVIO_LED_White = 7
+NAVIO_LED_Black = '0'
+NAVIO_LED_Red = '1'
+NAVIO_LED_Green = '2'
+NAVIO_LED_Blue = '3'
+NAVIO_LED_Yellow = '4'
+NAVIO_LED_Cyan = '5'
+NAVIO_LED_Magenta = '6'
+NAVIO_LED_White = '7'
 
 #-------------------NAVIO2 initialization--------------------------------
 led = navio.leds.Led()
